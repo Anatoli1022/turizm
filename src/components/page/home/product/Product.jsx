38781c26dcb19f3f4390bc743ee1555da0a7ebf7
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-05 22:51:52 +0300

    feat: adds section product

--- src/components/page/home/product/Product.jsx
+++ src/components/page/home/product/Product.jsx
@@ -1,8 +1,7 @@
 import { Navigation, Pagination, A11y } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-import products from '../../../../data/products';
+import React, { useEffect, useState } from 'react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -12,10 +11,29 @@ import 'swiper/css/scrollbar';
 
 import classNames from 'classnames/bind';
 import styles from './Product.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Product = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState('error 404 , page not found');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('http://turizm.atwebpages.com/index.php');
+        const info = await res.json();
+        setData(info);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <section className={cx('product')}>
       <div className={cx('container')}>
@@ -29,18 +47,20 @@ const Product = () => {
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log('slide change')}
         >
-          {products.slice(0, 6).map((product, index) => {
+          {data.slice(0, 6).map((product, index) => {
             return (
               <SwiperSlide key={index}>
-                <div className={cx('product-container')}>
-                  <img className={cx('image')} alt="" src={product.image} />
+                <Link to={`products/${product.slug}`}>
+                  <div className={cx('product-container')}>
+                    <img className={cx('image')} alt="" src={product.image} />
 
-                  <div className={cx('information-wrapper')}>
-                    <h3 className={cx('title')}> {product.slag}</h3>
-                    <p className={cx('information')}>{product.lastName}</p>
-                    <p className={cx('price')}>{product.email}</p>
+                    <div className={cx('information-wrapper')}>
+                      <h3 className={cx('title')}> {product.slug}</h3>
+                      <p className={cx('information')}>{product.lastName}</p>
+                      <p className={cx('price')}>{product.email}</p>
+                    </div>
                   </div>
-                </div>
+                </Link>
               </SwiperSlide>
             );
           })}

0c1dcb602c92d56373b5d695e54fbb01de9c5445
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-21 15:23:07 +0300

    feat: adds section footer

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+
+import facebook from '../../../images/facebook.svg';
+import messenger from '../../../images/messenger.svg';
+import tiktok from '../../../images/tiktok.svg';
+import whatsapp from '../../../images/whatsapp.svg';
+
+const cx = classNames.bind(styles);
+
+const Footer = () => {
+  return (
+    <footer className={cx('footer')}>
+      <div className={cx('container', 'footer-container')}>
+        <div className={cx('contacts')}>
+          <h3 className={cx('title')}>Obsługa Klienta</h3>
+          <a href="tel:+48577141227" className={cx('link')}>+48 577 141 227</a>
+          <a href="mailto:zapolujna.wakacje@gmail.com" className={cx('link')}>
+            zapolujna.wakacje@gmail.com
+          </a>
+        </div>
+        <div className={cx('social')}>
+          <h3 className={cx('title')}>Bądź na bieżąco</h3>
+          <div className={cx('icons')}>
+            <a href="https://www.facebook.com/people/Zapoluj-Na-Wakacje/pfbid05VYFWrf76kDrYQhT25MgF9HkS7goiqZMKvWQmWZ1mMY9tAvQMhwEoiXehuUzxd9bl/">
+              <img src={facebook} loading="lazy" alt="facebook" />
+            </a>
+            <a href="https://www.messenger.com/t/100010889255784">
+              <img src={messenger} loading="lazy" alt="facebook-messenger" />
+            </a>
+            <a href="https://www.tiktok.com/@zapolujna.wakacje?_t=8dqsGATSgRu&_r=1">
+              <img src={tiktok} loading="lazy" alt="tiktok" />
+            </a>
+
+            <a href="https://wa.me/+48577141227">
+              <img src={whatsapp} loading="lazy" alt="whatsapp" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layouts/MainLoyout.jsx
+++ src/layouts/MainLoyout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/shared/header/Header';
+import Footer from '../components/shared/footer/Footer';
 
 const MainLayout = () => {
   return (
     <>
       <Header />
+
       <Outlet />
+      <Footer />
     </>
   );
 };

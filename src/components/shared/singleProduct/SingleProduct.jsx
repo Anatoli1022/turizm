09c3257242731c0ba4dd00763855e55ded2dfc4e
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-24 18:57:53 +0300

    feat: adds galery from sungle page

--- src/components/shared/singleProduct/SingleProduct.jsx
+++ src/components/shared/singleProduct/SingleProduct.jsx
@@ -1,6 +1,16 @@
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import classNames from 'classnames/bind';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+import { FreeMode, Navigation, Thumbs } from 'swiper';
+
 import styles from './SingleProduct.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,6 +18,7 @@ const cx = classNames.bind(styles);
 const SingleProduct = () => {
   const [data, setData] = useState([]);
   const [error, setError] = useState('');
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,6 +33,7 @@ const SingleProduct = () => {
 
     fetchData();
   }, []);
+  console.log(data);
   const params = useParams();
   // const navigate = useNavigate();
 
@@ -34,13 +46,103 @@ const SingleProduct = () => {
   // }, [products, navigate]);
 
   return (
-    <div className={cx('product-wrapper')}>
-      <h1>{products?.lastName}</h1>
-      <h2>{products?.slug}</h2>
-      <h3>{products?.id}</h3>
-      <Link to=".." relative="path">
-        All Products
-      </Link>
+    <div className={cx('container', 'container-single-product')}>
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        }}
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper2"
+      >
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={products?.image}
+            alt=""
+            className={cx('image')}
+            loading="eager"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <div className={cx('product-wrapper')}>
+        <p className={cx('description')}>{products?.description}</p>
+        <h2>{products?.slug}</h2>
+
+        <Link to=".." relative="path">
+          All Products
+        </Link>
+      </div>
     </div>
   );
 };

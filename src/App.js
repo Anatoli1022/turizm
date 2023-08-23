c205509b18e6e3abab956bb097d16b2fa61ce04f
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-23 19:23:21 +0300

    feat: app js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLoyout from './layouts/MainLoyout';
+import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import SingleProduct from './components/shared/singleProduct/SingleProduct';
+import IndividualOffer from './pages/IndividualOffer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainLoyout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="products" element={<Products />} />
+          <Route path="individualoffer" element={<IndividualOffer />} />
           <Route path="products/:slug" element={<SingleProduct />} />
         </Route>
       </Routes>

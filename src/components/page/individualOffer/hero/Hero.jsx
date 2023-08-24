a4bb5a9fec89ce7df29c0c7d26946f0503bf15b0
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-24 15:24:30 +0300

    feat: adds section hero in page individualOffer

--- src/components/page/individualOffer/hero/Hero.jsx
+++ src/components/page/individualOffer/hero/Hero.jsx
@@ -1,66 +1,130 @@
 import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Hero.module.scss';
+import emailjs from 'emailjs-com'; // Import the EmailJS library
 
 const cx = classNames.bind(styles);
 
 const Hero = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState(''),
+    [email, setEmail] = useState(''),
+    [when, setWhen] = useState(''),
+    [direction, setDirection] = useState(''),
+    [people, setPeople] = useState(''),
+    [dining, setDining] = useState(''),
+    [budget, setBudget] = useState(''),
+    [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const formData = {
-      name,
-      email,
-      message,
-    };
-
-    const response = await fetch(
-      'https://turizm123.000webhostapp.com/php/mail.php',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+    // Send email using EmailJS
+    try {
+      await emailjs.send(
+        'service_z87utcq', // Your EmailJS service ID
+        'template_re54j3t', // Your EmailJS template ID
+        {
+          name: name,
+          email: email,
+          when: when,
+          direction: direction,
+          people: people,
+          dining: dining,
+          budget: budget,
+          message: message,
         },
-        body: JSON.stringify(formData),
-      }
-    );
+        'kL9gMMPRUTzr5AXLK' // Your EmailJS user ID
+      );
 
-    const data = await response.json();
-
-    if (response.ok) {
-      console.log(data.message);
-    } else {
-      console.error(data.message);
+      console.log('Email sent successfully!');
+    } catch (error) {
+      console.error('Error sending email:', error);
     }
+
+    setName('');
+    setEmail('');
+    setWhen('');
+    setDirection('');
+    setPeople('');
+    setDining('');
+    setBudget('');
+    setMessage('');
   };
 
   return (
     <section className={cx('hero')}>
-      <div className={cx('container')}>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <textarea
-            placeholder="Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
+      <div className={cx('container', 'hero-container')}>
+        <div className={cx('wrapper-text')}>
+          <h1 className={cx('title')}>
+            Napisz do nas, a my dobierzemy ofertę{' '}
+            <span className={cx('title-span')}>specjalnie dla ciebie</span>
+          </h1>
+        </div>
+        <div className={cx('wrapper-form')}>
+          <form onSubmit={handleSubmit} className={cx('form')}>
+            <div className={cx('wrapper-input')}>
+              <input
+                className={cx('form-input', 'input-name')}
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className={cx('form-input', 'input-email')}
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <input
+              className={cx('form-input')}
+              type="text"
+              placeholder="Kiedy"
+              value={when}
+              onChange={(e) => setWhen(e.target.value)}
+            />
+            <input
+              className={cx('form-input')}
+              type="text"
+              placeholder="Preferowane kierunki"
+              value={direction}
+              onChange={(e) => setDirection(e.target.value)}
+            />
+            <input
+              className={cx('form-input')}
+              type="text"
+              placeholder="Ile osób"
+              value={people}
+              onChange={(e) => setPeople(e.target.value)}
+            />
+            <input
+              className={cx('form-input')}
+              type="text"
+              placeholder="Preferowane opcji wyżywienia"
+              value={dining}
+              onChange={(e) => setDining(e.target.value)}
+            />
+            <input
+              className={cx('form-input')}
+              type="text"
+              placeholder="Budżet"
+              value={budget}
+              onChange={(e) => setBudget(e.target.value)}
+            />
+
+            <textarea
+              className={cx('message')}
+              placeholder="Dodatkowe uwagi"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit" className={cx('button')}>
+              wysłać
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );

cb602c845202a1c3c6f88094600fd6f3f24bb24a
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-27 16:03:22 +0300

    feat: adds section hero in page contact

--- src/components/page/individualOffer/hero/Hero.jsx
+++ src/components/page/individualOffer/hero/Hero.jsx
@@ -8,7 +8,8 @@ const cx = classNames.bind(styles);
 const Hero = () => {
   const [name, setName] = useState(''),
     [email, setEmail] = useState(''),
-    [when, setWhen] = useState(''),
+    [when_od, setWhen_od] = useState(''),
+    [when_do, setWhen_do] = useState(''),
     [direction, setDirection] = useState(''),
     [people, setPeople] = useState(''),
     [dining, setDining] = useState(''),
@@ -26,7 +27,8 @@ const Hero = () => {
         {
           name: name,
           email: email,
-          when: when,
+          when_od: when_do,
+          when_do: when_do,
           direction: direction,
           people: people,
           dining: dining,
@@ -43,7 +45,8 @@ const Hero = () => {
 
     setName('');
     setEmail('');
-    setWhen('');
+    setWhen_od('');
+    setWhen_do('');
     setDirection('');
     setPeople('');
     setDining('');
@@ -57,34 +60,49 @@ const Hero = () => {
         <div className={cx('wrapper-text')}>
           <h1 className={cx('title')}>
             Napisz do nas, a my dobierzemy ofertę{' '}
-            <span className={cx('title-span')}>specjalnie dla ciebie</span>
+            <span className={cx('title-span')}>specjalnie dla Ciebie</span>
           </h1>
         </div>
         <div className={cx('wrapper-form')}>
           <form onSubmit={handleSubmit} className={cx('form')}>
             <div className={cx('wrapper-input')}>
               <input
-                className={cx('form-input', 'input-name')}
+                className={cx('form-input')}
                 type="text"
                 placeholder="Name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
               <input
-                className={cx('form-input', 'input-email')}
+                className={cx('form-input')}
                 type="email"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
-            <input
-              className={cx('form-input')}
-              type="text"
-              placeholder="Kiedy"
-              value={when}
-              onChange={(e) => setWhen(e.target.value)}
-            />
+            <div className={cx('wrapper-date')}>
+              <p className={cx('text')}>Kiedy: </p>
+              <label className={cx('label-date')}>
+                <span className={cx('span-date')}>od</span>
+                <input
+                  className={cx('form-input', 'form-input-date')}
+                  type="date"
+                  value={when_od}
+                  onChange={(e) => setWhen_od(e.target.value)}
+                />
+              </label>{' '}
+              <label className={cx('label-date')}>
+                <span className={cx('span-date')}>do</span>
+                <input
+                  className={cx('form-input', 'form-input-date')}
+                  type="date"
+                  value={when_do}
+                  onChange={(e) => setWhen_do(e.target.value)}
+                />{' '}
+              </label>
+            </div>
             <input
               className={cx('form-input')}
               type="text"

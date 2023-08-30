a83f1cb9dab00b9f192f261b0e7fa4d49dd29211
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-30 23:22:34 +0300

    fix:loading

--- src/components/hooks/loading/Loading.js
+++ src/components/hooks/loading/Loading.js
@@ -8,14 +8,14 @@ const cx = classNames.bind(styles);
 const Loading = () => (
   <div className={cx('loading')}>
     <div className={cx('container')}>
-      <svg viewBox="0 0 100 100" class="spinner css-rpvu3e" className={cx('loading-svg')}>
+      <svg viewBox="0 0 100 100" className={cx('loading-svg')}>
         <circle
           cx="50"
           cy="50"
           r="45"
           fill="none"
           stroke="currentColor"
-          stroke-width="8"
+          strokeWidth="8"
         ></circle>
       </svg>
     </div>

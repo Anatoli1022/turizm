b7c7fc2b4dd0fa8c857315a752b9b3bf03c59898
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-30 23:26:01 +0300

    delete: delete comments

--- src/components/page/home/product/Product.jsx
+++ src/components/page/home/product/Product.jsx
@@ -48,33 +48,7 @@ const Product = () => {
           onSlideChange={() => console.log('slide change')}
         >
           {data.slice(0, 6).map((product, index) => {
-            // const isProductAlreadyRendered = data.some(
-            //   (item, currentIndex) =>
-            //     item.slug === product.slug && currentIndex < index
-            // );
-            // if (!isProductAlreadyRendered) {
-            //   return (
-            //     <SwiperSlide key={index}>
-            //       <Link to={`products/${product.slug}`} className={cx('link')}>
-            //         <div className={cx('product-container')}>
-            //           <img
-            //             className={cx('image')}
-            //             alt=""
-            //             src={product.main_link}
-            //           />
-            //           <h2 className={cx('image-title')}>{product.to}</h2>
-            //           <div className={cx('information-wrapper')}>
-            //             <h3 className={cx('card-title')}> {product.to}</h3>
-            //             <p className={cx('information')}>
-            //               {product.description}
-            //             </p>
-            //             <p className={cx('text')}>Dowiedz się więcej!</p>
-            //           </div>
-            //         </div>
-            //       </Link>
-            //     </SwiperSlide>
-            //   );
-            // }
+         
             return (
               <SwiperSlide key={index}>
                 <Link to={`products/${product.slug}`} className={cx('link')}>

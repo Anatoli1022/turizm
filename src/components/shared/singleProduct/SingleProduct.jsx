ddc4d77f7827f87a2b47ee22ced5cf3e30f16afc
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-30 23:25:22 +0300

    feat:  page  SingleProduct

--- src/components/shared/singleProduct/SingleProduct.jsx
+++ src/components/shared/singleProduct/SingleProduct.jsx
@@ -42,8 +42,10 @@ const SingleProduct = () => {
 
   const products = data.filter((data) => data.slug == params.slug);
 
+  //single info for one product
   const singleInformation = products[0];
 
+  //reverse data
   const formatDate = (dateString) => {
     if (!dateString) {
       return '';
@@ -58,107 +60,146 @@ const SingleProduct = () => {
   const formattedDepartureDate = formatDate(singleInformation?.departure_date);
   const formattedReturnDate = formatDate(singleInformation?.return_date);
 
+  //checkbox
+  const [checked, setChecked] = useState(false);
+
+  function chengeCheckbox() {
+    setChecked(!checked);
+  }
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <div className={cx('single-product')}>
-      <div className={cx('container', 'wrapper')}>
-        <Swiper
-          style={{
-            '--swiper-navigation-color': '#fff',
-            '--swiper-pagination-color': '#fff',
-          }}
-          loop={true}
-          spaceBetween={10}
-          navigation={true}
-          thumbs={{ swiper: thumbsSwiper }}
-          modules={[FreeMode, Navigation, Thumbs]}
-          className="mySwiper2"
-        >
-          <SwiperSlide>
-            <img
-              src={singleInformation.main_link}
-              alt=""
-              className={cx('image')}
-              loading="eager"
-            />
-          </SwiperSlide>
-
-          {products.map((product, i) => {
-            return (
-              <SwiperSlide key={i}>
-                <img
-                  src={product.link}
-                  alt=""
-                  className={cx('image')}
-                  loading="eager"
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          loop={true}
-          spaceBetween={10}
-          slidesPerView={4}
-          freeMode={true}
-          watchSlidesProgress={true}
-          modules={[FreeMode, Navigation, Thumbs]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img
-              src={singleInformation.main_link}
-              alt=""
-              className={cx('image')}
-              loading="eager"
-            />
-          </SwiperSlide>
-
-          {products.map((product, i) => {
-            return (
-              <SwiperSlide key={i}>
-                <img
-                  src={product.link}
-                  alt=""
-                  className={cx('image')}
-                  loading="eager"
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-
-        <div className={cx('product-wrapper')}>
+      <div className={cx('container-large', 'wrapper')}>
+        <div className={cx('single-product-container')}>
           <div>
-            <h3 className={cx('title-place')}>
-              {`${singleInformation.from} - ${singleInformation.to}`}
-            </h3>
-            <div className={cx('container-date')}>
-              <span className={cx('date')}>
-                <span>Wylot:</span>
-                <span className={cx('date-number')}>
-                  {formattedDepartureDate}
-                </span>
-              </span>
-              <span className={cx('date')}>
-                <span> Powrót:</span>
-                <span className={cx('date-number')}>{formattedReturnDate}</span>
-              </span>
-            </div>
-            <span className={cx('price')}>
-              <span> Cena za osobę </span>{' '}
-              <span className={cx('price-number')}>
-                {singleInformation.price}zł
-              </span>
-            </span>
+            <Swiper
+              style={{
+                '--swiper-navigation-color': '#fff',
+                '--swiper-pagination-color': '#fff',
+              }}
+              loop={true}
+              spaceBetween={10}
+              navigation={true}
+              thumbs={{ swiper: thumbsSwiper }}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper2"
+            >
+              <SwiperSlide>
+                <img
+                  src={singleInformation.main_link}
+                  alt=""
+                  className={cx('image')}
+                  loading="eager"
+                />
+              </SwiperSlide>
+
+              {products.map((product, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <img
+                      src={product.link}
+                      alt=""
+                      className={cx('image')}
+                      loading="eager"
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+            <Swiper
+              onSwiper={setThumbsSwiper}
+              loop={true}
+              spaceBetween={10}
+              slidesPerView={4}
+              freeMode={true}
+              watchSlidesProgress={true}
+              modules={[FreeMode, Navigation, Thumbs]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img
+                  src={singleInformation.main_link}
+                  alt=""
+                  className={cx('image')}
+                  loading="eager"
+                />
+              </SwiperSlide>
+
+              {products.map((product, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <img
+                      src={product.link}
+                      alt=""
+                      className={cx('image')}
+                      loading="eager"
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </div>
-          <button className={cx('button-buy')}>Kupić</button>
-          <p className={cx('description')}>{singleInformation.description}</p>
+
+          <div className={cx('product-wrapper')}>
+            <div>
+              <h3 className={cx('title-place')}>
+                {`${singleInformation.from} - ${singleInformation.to}`}
+              </h3>
+              <div className={cx('container-date')}>
+                <span className={cx('date')}>
+                  <span>Wylot:</span>
+                  <span className={cx('date-number')}>
+                    {formattedDepartureDate}
+                  </span>
+                </span>
+                <span className={cx('date')}>
+                  <span> Powrót:</span>
+                  <span className={cx('date-number')}>
+                    {formattedReturnDate}
+                  </span>
+                </span>
+              </div>
+              <span className={cx('price')}>
+                <span> Cena za osobę </span>{' '}
+                <span className={cx('price-number')}>
+                  {singleInformation.price}zł
+                </span>
+              </span>{' '}
+              <button
+                className={cx('button-buy', checked ? 'button-active' : null)}
+              >
+                Kupić
+              </button>
+              <div className={cx('wrapper-payment-information')}>
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={checked}
+                    onChange={chengeCheckbox}
+                    className={cx('payment-checkbox')}
+                  />
+                  <label className={cx('checkbox-text')}>
+                    <span className={cx('payment-star')}>*</span>
+                    Oświadczam, że zapoznałem się z następującymi informacjami
+                    dotyczącymi płatności za tę ofertę
+                  </label>
+                </div>
+
+                <p className={cx('payment-text')}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  repellat, id alias dolores delectus reiciendis voluptates
+                  illum harum illo, inventore eveniet voluptatem recusandae
+                  laboriosam, dolorem maxime! In odio voluptate cupiditate.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <p className={cx('description')}>{singleInformation.description}</p>
         <Link to=".." relative="path" className={cx('link')}>
           All Products
         </Link>

6a9c91b4958fa4f08611c4c88ea3d61384e3442b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-03 23:05:39 +0300

    feat: adds new sections for page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Hero from '../components/page/home/hero/Hero';
 import Product from '../components/page/home/product/Product';
+import AboutUs from '../components/page/home/aboutUs/AboutUs';
+import Questions from '../components/page/home/questions/Questions';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Product />
+      <AboutUs />
+      <Questions />
       <Outlet />
     </>
   );

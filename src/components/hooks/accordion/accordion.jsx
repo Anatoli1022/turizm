79c34690f75a87f2253e3da6b3f4e447cbbf39e3
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-09-03 23:00:16 +0300

    feat: adds hook accordion

---
+++ src/components/hooks/accordion/accordion.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './accordion.module.scss';
+import classNames from 'classnames/bind';
+import useOpen from '../open/open';
+
+const cx = classNames.bind(styles);
+
+export const Accordion = ({ section, key }) => {
+  const { isOpen, toggle } = useOpen(false);
+
+  return (
+    <li className={cx('item', { active: isOpen == true })}>
+      <Title question={section.question} isOpen={isOpen} toggle={toggle} />
+      <Text text={section.text} />
+    </li>
+  );
+};
+
+export const Title = ({ question, toggle }) => {
+  return (
+    <h3 className={cx('item-title')} onClick={toggle}>
+      {question}
+    </h3>
+  );
+};
+
+export const Text = ({ text }) => {
+  return <p className={cx('item-text')}>{text}</p>;
+};

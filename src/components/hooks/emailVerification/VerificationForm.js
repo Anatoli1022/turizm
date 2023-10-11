4c2be2fbd31e2203f1bc969d1f94f14774afeecd
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-10-11 21:51:02 +0300

    delete: delete useEffect in VerificationForm

--- src/components/hooks/emailVerification/VerificationForm.js
+++ src/components/hooks/emailVerification/VerificationForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import styles from './VerificationForm.module.scss';
 import classNames from 'classnames/bind';

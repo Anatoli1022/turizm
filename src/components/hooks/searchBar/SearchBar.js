208b617602987fed0028336489363d2af475e9b4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-10-18 19:26:18 +0300

    feat: adds timeOut for searchBar

--- src/components/hooks/searchBar/SearchBar.js
+++ src/components/hooks/searchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './SearchBar.module.scss';
 import search from '../../../images/search.svg';
@@ -6,16 +6,29 @@ import search from '../../../images/search.svg';
 const cx = classNames.bind(styles);
 
 const SearchBar = ({ data, setFilteredData }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const delay = 500;
+
+  let debounceTimeout;
+
   const handleSearch = (event) => {
     const keyword = event.target.value.toLowerCase();
-    const filteredData = data.filter(
-      (item) =>
-        //where the search will be performed
-        item.to.toLowerCase().includes(keyword) ||
-        item.departure_date.toLowerCase().includes(keyword) ||
-        item.from.toLowerCase().includes(keyword)
-    );
-    setFilteredData(filteredData);
+    setSearchTerm(keyword);
+
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    debounceTimeout = setTimeout(() => {
+      const filteredData = data.filter(
+        (item) =>
+          //where the search will be performed
+          item.to.toLowerCase().includes(keyword) ||
+          item.departure_date.toLowerCase().includes(keyword) ||
+          item.from.toLowerCase().includes(keyword)
+      );
+      setFilteredData(filteredData);
+    }, delay);
   };
 
   return (
@@ -24,6 +37,7 @@ const SearchBar = ({ data, setFilteredData }) => {
         type="text"
         className={cx('search')}
         placeholder="Wyszukiwanie"
+        value={searchTerm}
         onChange={handleSearch}
       />
       <img

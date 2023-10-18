85807439fda637509d05ac13ab1abb967fe84bcc
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-10-19 00:43:02 +0300

    feat: adds new pages for App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './pages/Home';
 import Products from './pages/Products';
 import SingleProduct from './components/shared/singleProduct/SingleProduct';
 import IndividualOffer from './pages/IndividualOffer';
-
+import PaymentSuccessPage from './pages/PaymentSuccessPage'; 
+import FailedPayment from './pages/FailedPaymentPage.jsx';
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,8 @@ function App() {
           <Route index element={<Home />} />
           <Route path="products" element={<Products />} />
           <Route path="individualoffer" element={<IndividualOffer />} />
+          <Route path="paymentsuccessage" element={<PaymentSuccessPage />} /> 
+          <Route path="failedpayment" element={<FailedPayment />} />
           <Route path="products/:slug" element={<SingleProduct />} />
         </Route>
       </Routes>

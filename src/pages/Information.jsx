4a33dceda530af8e4f5e9bd5418b73364eccfce5
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-11-16 23:43:52 +0300

    feat:add new page information and new component aboutUs

---
+++ src/pages/Information.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AboutUs from '../components/page/information/aboutUs/AboutUs';
+const Information = () => {
+  return (
+    <>
+      <AboutUs />
+    </>
+  );
+};
+
+export default Information;
